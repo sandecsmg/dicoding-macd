914d7db38094091f32cd45f3dd19c06f29abb588
sandecsmg <33228494+sandecsmg@users.noreply.github.com>
2019-09-19 16:03:27 +0700

    Delete created_at

--- add_users.php
+++ add_users.php
@@ -6,11 +6,10 @@
         $address = $_POST['address'];
         $phone = $_POST['phone'];
         $uid = md5($name.$phone);
-        $created = date('Y-m-d h:i:s');
     
-        $params = array($name, $email, $address, $phone, $uid, $created);
+        $params = array($name, $email, $address, $phone, $uid);
         try {
-            $query = sqlsrv_query($conn, "INSERT INTO users (name, email, address, phone, user_id, created_at) VALUES (?, ?, ?, ?, ?, ?)", $params);        
+            $query = sqlsrv_query($conn, "INSERT INTO users (name, email, address, phone, user_id) VALUES (?, ?, ?, ?, ?)", $params);        
         }
         catch (Exception $e) {
             echo $e;
