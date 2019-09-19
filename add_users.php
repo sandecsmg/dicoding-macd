0bf83ae5aece3daa73c98213f17b279be9b67e43
sandecsmg <salam@sandec.org>
2019-09-19 16:19:37 +0700

    More error handling

--- add_users.php
+++ add_users.php
@@ -1,20 +1,24 @@
 <?php
     include_once('env.php');
-    if(isset($_POST['name']) || isset($_POST['email'])) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $address = $_POST['address'];
-        $phone = $_POST['phone'];
-        $uid = md5($name.$phone);
+    try {
+        if (isset($_POST['name']) || isset($_POST['email'])) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $address = $_POST['address'];
+            $phone = $_POST['phone'];
+            $uid = md5($name.$phone);
     
-        $params = array($name, $email, $address, $phone, $uid);
-        try {
-            $query = sqlsrv_query($conn, "INSERT INTO users (name, email, address, phone, user_id) VALUES (?, ?, ?, ?, ?)", $params);        
+            $params = array($name, $email, $address, $phone, $uid);
+            try {
+                $query = sqlsrv_query($conn, "INSERT INTO users (name, email, address, phone, user_id) VALUES (?, ?, ?, ?, ?)", $params);
+            } catch (Exception $e) {
+                echo $e;
+            }
+            sqlsrv_free_stmt($query);
+            header("Location: index.php");
         }
-        catch (Exception $e) {
-            echo $e;
-        }
-        sqlsrv_free_stmt($query); 
-        header("Location: index.php");
     }
+catch(Exception $e) {
+    echo $e;
+}
 ?>
